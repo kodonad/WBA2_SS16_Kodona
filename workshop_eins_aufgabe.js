d4491f7f248ee13c637eb1f01dbc2d838e5c9c73
Dennis Dick <dennis.dick@smail.th-koeln.de>
2016-04-30 11:53:22 +0200

    Aufgabe 1 mit Kommentaren versehen.

--- workshop_eins_aufgabe.js
+++ workshop_eins_aufgabe.js
@@ -7,14 +7,15 @@ if (err) {
 	throw err;
 	}
 
-content = JSON.parse(data);
+content = JSON.parse(data); // Der Variabel content werden die ausgelesenen Informationen der Datei hinzugewiesen.
+			   // Das Parse sorgt dafür, das die Informationen der Datei zu einem Objekt umgewandelt werden.
 
-for(i = 0; i < content.wolkenkratzer.length; i++)
+for(i = 0; i < content.wolkenkratzer.length; i++) // Die For-Schleife dient hier zur strukturierten Ausgabe des Objektes.
 {
-	console.log("\nName: "+content.wolkenkratzer[i].name);
-	console.log("Stadt: "+content.wolkenkratzer[i].stadt);
-	console.log("Höhe: "+content.wolkenkratzer[i].hoehe);
-	console.log("\n------------------------------------");
+	console.log("\nName: "+content.wolkenkratzer[i].name);		// Beispiel:  Name : Burj Khalifa
+	console.log("Stadt: "+content.wolkenkratzer[i].stadt);		//  	      Stadt: Dubai
+	console.log("Höhe: "+content.wolkenkratzer[i].hoehe);		//	      Höhe : 828	
+	console.log("\n------------------------------------");		//           -------------------------
 }
 
 });
