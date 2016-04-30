ad8a85b5029f6588dd33934fc6ae0041e84cc5c8
Dennis Dick <dennis.dick@smail.th-koeln.de>
2016-04-30 13:08:25 +0200

    Datei mit Aufgabe3 erweitert

--- workshop_eins_aufgabe.js
+++ workshop_eins_aufgabe.js
@@ -11,13 +11,32 @@ if (err) {
 content = JSON.parse(data); // Der Variabel content werden die ausgelesenen Informationen der Datei hinzugewiesen.
 			   // Das Parse sorgt dafür, das die Informationen der Datei zu einem Objekt umgewandelt werden.
 
-for(i = 0; i < content.wolkenkratzer.length; i++) // Die For-Schleife dient hier zur strukturierten Ausgabe des Objektes.
-{
-	console.log("\nName: "+chalk.red(content.wolkenkratzer[i].name));		// Beispiel:  Name : Burj Khalifa
-	console.log("Stadt: "+chalk.cyan(content.wolkenkratzer[i].stadt));		//  	      Stadt: Dubai
-	console.log("Höhe: "+chalk.green(content.wolkenkratzer[i].hoehe));		//	      Höhe : 828	
-	console.log("\n------------------------------------");		//           ------------------------
-}
+
+content.wolkenkratzer.sort(function(a,b) {
+
+if(a.hoehe < b.hoehe) return 1;
+
+if(a.hoehe > b.hoehe) return -1;
+
+else return 0;
 
 });
+
+
+fs.writeFile(__dirname+"/wolkenkratzer_sortiert.json",JSON.stringify(content),function(err) {
+
+if (err) throw err;
+
+for(i = 0; i < content.wolkenkratzer.length; i++) // Die For-Schleife dient hier zur strukturierten Ausgabe des Objektes.
+{
+        console.log("\nName: "+chalk.red(content.wolkenkratzer[i].name));               // Beispiel:  Name : Burj Khalifa
+	console.log("Stadt: "+chalk.cyan(content.wolkenkratzer[i].stadt));              //            Stadt: Dubai
+	console.log("Höhe: "+chalk.green(content.wolkenkratzer[i].hoehe));	        //	      Höhe : 828
+	console.log("\n--------------------------------------");			//	      --------------------
+}
+
+
+});
+});
+
 
